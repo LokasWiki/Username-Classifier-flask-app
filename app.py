2f9966e10530b322ad36ae0e713d4307ac78e89b
lokas <lokashafeek7755@gmail.com>
2023-03-23 22:06:39 +0200

    add auth key

--- app.py
+++ app.py
@@ -1,37 +1,41 @@
 import os
-
+import configparser
 from markupsafe import escape
 from transformers import pipeline
-
-from flask import Flask, render_template, request, url_for, abort, request, url_for, flash, redirect
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 __dir__ = os.path.dirname(__file__)
 
+# Initialize the spam classifier outside of the Flask application
 spam_classifier_generator = pipeline(model="lokas/spam-usernames-classifier")
+
+# Read the API key from the config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_key = config['API']['key']
 
 
 @app.route('/')
 def index():
-    return {
-        "status": "working"
-    }
+    return {"status": "working"}
 
 
-# Define a route for making predictions
-# todo:add api key
 @app.route('/predict', methods=['POST'])
 def predict_view():
+    # Check if the API key is present in the request header
+    api_key_header = request.headers.get('X-Api-Key')
+    if api_key_header != api_key:
+        return {"message": "Unauthorized"}, 401
+
+    # Extract the list of usernames from the request body
     usernames = request.json.get('usernames', [])
+
+    # Run the spam classifier on each username and return the results
     predictions = spam_classifier_generator(usernames)
-    result = []
-    for username, prediction in zip(usernames, predictions):
-        result.append({
-            "username": username,
-            "prediction": prediction,
-        })
-    return {"result": result}
+    results = [{"username": username, "prediction": prediction} for username, prediction in zip(usernames, predictions)]
+    return {"results": results}
 
 
 if __name__ == "main":
