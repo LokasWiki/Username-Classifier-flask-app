2c951a60fef6ba77ea943bd7d49541053aa79be0
lokas <lokashafeek7755@gmail.com>
2023-03-23 22:00:04 +0200

    make it take more users not one

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ __dir__ = os.path.dirname(__file__)
 spam_classifier_generator = pipeline(model="lokas/spam-usernames-classifier")
 
 
-
-
 @app.route('/')
 def index():
     return {
@@ -25,12 +23,16 @@ def index():
 # todo:add api key
 @app.route('/predict', methods=['POST'])
 def predict_view():
-    username = request.form['username']
-    prediction = spam_classifier_generator([username])
-    return {
-        "username": username,
-        "prediction": prediction,
-    }
+    usernames = request.json.get('usernames', [])
+    predictions = spam_classifier_generator(usernames)
+    result = []
+    for username, prediction in zip(usernames, predictions):
+        result.append({
+            "username": username,
+            "prediction": prediction,
+        })
+    return {"result": result}
+
 
 if __name__ == "main":
     app.run()
